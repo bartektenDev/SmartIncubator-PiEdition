a0333871f38c2bf89d7eacddf7bd522bae5c7dc8
bartektenDev <bartek8991@live.com>
2020-02-18 16:59:09 -0600

    updated ui

--- public_html/index.php
+++ public_html/index.php
@@ -2,8 +2,8 @@
 
 $loadPage = location.href;
 
-if(strpos($loadPage, "index.php?light=") !== true){
-  header("Location: incubator.php?light=Unknown");
+if(strpos($loadPage, "index.php") !== true){
+  header("Location: incubator.php");
 }else{
 
 }
--- public_html/settings.php
+++ public_html/settings.php
@@ -13,6 +13,9 @@ if ("password" == $password) {
       <!--Import materialize.css-->
       <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
       <link rel="stylesheet" href="./css/animate.css">
+      <link rel="icon"
+      type="image/png"
+      href="./images/earthboxicon.png" />
       <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
